70bcab0a56a099c136954ea9777d96ce16efbd60
Andres Camilo Ocampo Ramirez <andres_ocampo82211@elpoli.edu.co>
2025-08-24 15:22:43 -0500

    test(order): Implement unit tests in order use case [feature/hu-11/make-order]

--- src/main/java/com/pragma/powerup/application/dto/request/OrderRequestDto.java
+++ src/main/java/com/pragma/powerup/application/dto/request/OrderRequestDto.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.application.dto.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
--- src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
@@ -1,6 +1,7 @@
 package com.pragma.powerup.domain.usecase;
 
 import com.pragma.powerup.domain.api.IOrderServicePort;
+import com.pragma.powerup.domain.exception.OrderInProcessException;
 import com.pragma.powerup.domain.model.OrderModel;
 import com.pragma.powerup.domain.model.OrderState;
 import com.pragma.powerup.domain.spi.IOrderPersistencePort;
@@ -17,6 +18,12 @@ public class OrderUseCase implements IOrderServicePort {
 
     @Override
     public void makeOrder(OrderModel order){
+        OrderModel lastUserOrder = orderPersistencePort.getOrderByUserId(order.getUserId());
+
+        if(lastUserOrder != null && !lastUserOrder.getState().equals(OrderState.DELIVERED.label)){
+            throw new OrderInProcessException();
+        }
+        
         order.setDate(LocalDateTime.now());
         order.setState(OrderState.PENDING.label);
         orderPersistencePort.makeOrder(order);
